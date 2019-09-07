b4a797f855c0c3e73b8660f0f3b3fb875c57c9ec
Anmol Jain <itsmeaj20@gmail.com>
2019-09-07 20:55:10 +0530

    Trying to add tab bar

---
+++ SwiftRadio/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  SwiftRadio
+//
+//  Created by Anmol Jain on 07/09/19.
+//  Copyright © 2019 matthewfecher.com. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.blue
+        self.title = "Tab 2"
+        print("Tab 2 Loaded")
+    }
+}
